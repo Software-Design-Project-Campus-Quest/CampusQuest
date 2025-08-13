2e7cec41008835599232f2bf933e3009d65759a2
Kuhlle-16 <2603232@students.wits.ac.za>
2025-08-13 20:08:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, 'static')));
+// Serve all static files from the current directory (where index.html is)
+app.use(express.static(__dirname));
+
+// For any other route, serve index.html (for React Router)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
